ecab9d36ec17a38b98d70b6b2ee8532f37683da3
rlopez-lab <74743209+rlopez-lab@users.noreply.github.com>
2020-12-07 17:52:57 -0600

    Creacion de Operaciones

---
+++ SobrecargaMetodos/src/main/java/Operaciones.java
@@ -0,0 +1,16 @@
+
+public class Operaciones {
+    
+    //Metodo sumar
+    public static int sumar(int a, int b){
+        System.out.println("metodo sumar(int, int)");
+        return a+b;
+    }
+    
+    //sobrecarga del metodo sumar
+    public static double sumar(double a, double b){
+        System.out.println("metodo sumar(double, double");
+        return a+b;
+    }
+    
+}
