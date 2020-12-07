c8f67b47e3285ecdb55c6f25718479335bb1dbb3
rlopez-lab <74743209+rlopez-lab@users.noreply.github.com>
2020-12-07 16:34:20 -0600

    Creacion clase empleado y mod Persona

---
+++ SobrecargaConstructores/src/main/java/Empleado.java
@@ -0,0 +1,30 @@
+
+public class Empleado extends Persona {
+    
+    private double sueldo;
+    
+    public Empleado(String nombre, int edad) {
+        super(nombre, edad);
+    }
+    
+    public Empleado(String nombre, int edad, double sueldo){
+        this(nombre, edad);
+        this.sueldo = sueldo;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Empleado{" + "sueldo=" + sueldo + '}';
+    }
+    
+    
+    
+}
--- SobrecargaConstructores/src/main/java/Persona.java
+++ SobrecargaConstructores/src/main/java/Persona.java
@@ -16,6 +16,32 @@ public class Persona {
         this.edad = edad;
     }
 
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public static int getContadorPersonas() {
+        return contadorPersonas;
+    }
+    
     
     @Override
     public String toString() {
