20080c07bb5bb60141e450c39c0e63be8bd2cd93
rlopez-lab <74743209+rlopez-lab@users.noreply.github.com>
2020-12-07 16:38:55 -0600

    Prueba sobrecarga constructores

---
+++ SobrecargaConstructores/src/main/java/SobrecargaConstructores.java
@@ -0,0 +1,7 @@
+
+public class SobrecargaConstructores {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan", 25);
+        System.out.println(persona1);
+    }
+}
