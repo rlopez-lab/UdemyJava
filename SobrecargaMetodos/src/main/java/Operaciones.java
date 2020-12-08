1256db63ae636b2aa11139c4d2d372ab6fee3c83
rlopez-lab <74743209+rlopez-lab@users.noreply.github.com>
2020-12-07 18:44:14 -0600

    prueba sobrecarga de metodos

--- SobrecargaMetodos/src/main/java/Operaciones.java
+++ SobrecargaMetodos/src/main/java/Operaciones.java
@@ -9,8 +9,18 @@ public class Operaciones {
     
     //sobrecarga del metodo sumar
     public static double sumar(double a, double b){
-        System.out.println("metodo sumar(double, double");
+        System.out.println("metodo sumar(double, double)");
         return a+b;
+    }
+    
+    public static double sumar(int a, double b){
+        System.out.println("metodo sumar(int, double)");
+        return a+b;
+    }
+    
+    public static double sumar(double a, int b){
+        System.out.println("metodo sumar(double, int)");
+        return a +b;
     }
     
 }
