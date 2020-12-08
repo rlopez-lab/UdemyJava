93cd05d26247f969e0e70fce68be0981d3e91792
rlopez-lab <74743209+rlopez-lab@users.noreply.github.com>
2020-12-08 11:44:38 -0600

    Creacion clases ClaseFinal, Persona

---
+++ EjemploPalabraFinal/src/main/java/palabrafinal/ClaseFinal.java
@@ -0,0 +1,16 @@
+
+package palabrafinal;
+
+public final class ClaseFinal {    
+    //Variable marcada como final
+    public final int varNumero = 10;
+    
+    //Constante en Java
+    public static final int VAR_PRIMITIVO =15;
+    public static final Persona VAR_PERSONA = new Persona();
+    
+    //Metodo final
+    
+    
+}
+
---
+++ EjemploPalabraFinal/src/main/java/palabrafinal/Persona.java
@@ -0,0 +1,16 @@
+
+package palabrafinal;
+
+public class Persona {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
