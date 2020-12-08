603ebcaf7a6c297ebe51dd76a1ed07c57238c942
rlopez-lab <74743209+rlopez-lab@users.noreply.github.com>
2020-12-08 10:45:29 -0600

    Impresion desde  paquete

--- EjemploPaquetes/src/main/java/EjemploPaquetesPrueba.java
+++ EjemploPaquetes/src/main/java/EjemploPaquetesPrueba.java
@@ -1,4 +1,13 @@
 
+import com.gm.Utileria;
+
+
+
 public class EjemploPaquetesPrueba {
-    
+    public static void main(String[] args) {
+        //utilizando el nombre de la clase completamente calificado
+        //com.gm.Utileria.imprimir("Hola");
+        
+        Utileria.imprimir("Hola");
+    }
 }
