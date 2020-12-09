6e3fadac06952b9d630e0000cef40ce811c2a2e9
rlopez-lab <74743209+rlopez-lab@users.noreply.github.com>
2020-12-08 18:33:59 -0600

    Prueba ejemplo arreglos

---
+++ EjemploArreglos/src/main/java/arreglos/EjemploArreglos.java
@@ -0,0 +1,26 @@
+
+package arreglos;
+
+public class EjemploArreglos {
+    public static void main(String[] args) {
+        int edades[];
+        edades = new int[3];
+        edades[0] = 30;
+        edades[1] = 15;
+        edades[2] = 20;
+        
+        System.out.println("arreglo de enteros indice 0: " + edades[0]);
+        System.out.println("arreglo de enteros indice 1: " + edades[1]);
+        System.out.println("arreglo de enteros indice 2: " + edades[2]);
+        
+        Persona personas[] = new Persona[4];
+        
+        personas[0] = new Persona("Juan");
+        personas[2] = new Persona("Karla");
+        
+        System.out.println("Arreglo personas indice 0: " + personas[0]);
+        System.out.println("Arreglo personas indice 1: " + personas[1]);
+        System.out.println("Arreglo personas indice 1: " + personas[2]);
+        System.out.println("Arreglo personas indice 1: " + personas[3]);
+    }
+}
