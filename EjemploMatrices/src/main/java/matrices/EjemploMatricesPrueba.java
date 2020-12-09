714b8e1237c513507c3efb6ebff6f3d424ac4674
rlopez-lab <74743209+rlopez-lab@users.noreply.github.com>
2020-12-09 12:08:54 -0600

    Matrices con objetos

--- EjemploMatrices/src/main/java/matrices/EjemploMatricesPrueba.java
+++ EjemploMatrices/src/main/java/matrices/EjemploMatricesPrueba.java
@@ -19,5 +19,23 @@ public class EjemploMatricesPrueba {
         System.out.println("Matriz enteros 0 0: " + edades[0][0]);
         System.out.println("Matriz enteros 2 0: " + edades[2][0]);
         
+         //1. Declarar e instanciar una matriz de objetos de tipo persona
+        Persona personas[][] = new Persona[1][2];
+        
+        //2. Inicializamos valores
+        personas[0][0] = new Persona("Juan");
+        personas[0][1] = new Persona("Karla");
+        
+        
+        //Imprimimos valores
+        System.out.println("Matriz persona indice 0 0: " + personas[0][0]);
+        System.out.println("Matriz persona indice 0 1: " + personas[0][1]);
+        
+        for(int renglon = 0; renglon < personas.length; renglon++){
+            for(int columna =0; columna < personas[renglon].length; columna++){
+                System.out.println("Matriz personas indice: " + renglon + " - " + columna + " - "+ personas[renglon][columna]);
+            }
+        }
+        
     }
 }
